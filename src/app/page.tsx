147691f364eabc665dda6ac3c5c9f8f960c2c478
Eduardo Becerra <josebecerra.0870@gmail.com>
2025-07-03 23:01:54 -0700

    First commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-2xl text-gray-600 font-bold">About me</h1>
+        <p className="mt-2 text-gray-600">Explore my projects and skills.</p>
+      </div>
+    </div>
+  );
+}
